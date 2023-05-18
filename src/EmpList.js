1b582ca32acd2e3d66a7ef8986d90a8d7a9625f6
ragahvendra23c <132534248+ragahvendra23c@users.noreply.github.com>
2023-05-18 15:12:05 +0530

    Update EmpList.js

--- src/EmpList.js
+++ src/EmpList.js
@@ -30,7 +30,7 @@ function EmpList() {
             })
                 .then(() => {
                     alert("Deleted successfully..!!")
-                    location.reload()
+                    window.location.reload(false)
                 })
         }
     }
@@ -92,7 +92,7 @@ function EmpList() {
             .then(() => {
                 alert("Logged out succefully..!")
                 navigate("/login")
-                location.reload()
+                window.location.reload(false)
             })
             .catch((err) => {
                 alert("error "+err)
